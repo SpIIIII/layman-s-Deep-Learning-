134910f7ad0734712d24265d5664562d95fb5caa
SpIIIII <46091793+SpIIIII@users.noreply.github.com>
2019-08-07 16:04:13 +0300

    daily

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -29,22 +29,43 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     # We will go through every dimension of x and compute numeric
     # derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    
     while not it.finished:
-        ix = it.multi_index
-        print(ix)
-        analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = (f(x+4)[0]-f(x-4)[0])/4
+      
+      ix = it.multi_index
+      zeros = np.zeros_like(x)
+      zeros[ix]=x[ix]
+      analytic_grad_at_ix = analytic_grad[ix]
+      
+      pp('f(',(x+2),')- f(',(x-2),')/',4,'   =')
+      pp(f(x+2)[0],'-', f(x-2)[0],'/',4,'   =',(f(x+2)[0]-f(x-2)[0])/4)
+      
+      numeric_grad_at_ix = (f(x+2)[0]-f(x-2)[0])/4
 
-        # TODO compute value of numeric gradient of f to idx
-        if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
-            return False
+      # TODO compute value of numeric gradient of f to idx
+      if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
+        print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+        return False
 
-        it.iternext()
+      it.iternext()
+    
+    if not analytic_grad == (f(x+2)[0]-f(x-2)[0])/4:
+      return False
 
     print("Gradient check passed!")
     return True
 
         
-
+def fmt_items(lines,max_lines=0):
+    max_width=max([len(line)for line in lines])
+    empty =' '*max_width
+    lines = [line.ljust(max_width)for line in lines]
+    lines += [empty]*(max_lines - len(lines))
+    return lines
+def pp (*list):
+    lines = [ str(item).split('\n') for item in list]
+    max_lines=max([len(item)for  item in lines])
+    lines = [fmt_items(item,max_lines=max_lines)for item in lines]
+    lines_t= np.array(lines).T
+    print('\n'.join([' '.join(line) for  line in lines_t]))
         
